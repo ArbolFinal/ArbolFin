ff229b8e86fb2ef91f96875454831824542e230d
Usuario <Usuario@DESKTOP-9OACGH9>
2018-04-10 04:26:55 -0500

    Agrege el metodo Abol Degenerado en la clase TreeSet

--- src/arbolfinal/TreeSet.java
+++ src/arbolfinal/TreeSet.java
@@ -1,4 +1,3 @@
-
 package arbolfinal;
 
 import java.util.AbstractSet;
@@ -9,41 +8,38 @@ import java.util.List;
 /*
 Implementa un arbol binario con caracteristicas de
 conjunto, por lo tanto no puede tener elementos repetidos 
-*/
-
+ */
 public class TreeSet<E extends Comparable<E>> extends AbstractSet<E> {
-    
-	
-	// define el nodo del arbol
+
+    // define el nodo del arbol
     private static class Node<E> {
+
         public Node<E> left;
         public Node<E> right;
         public E info;
-	
-		// constructor del nodo
+
+        // constructor del nodo
         public Node(E info) {
             this.info = info;
             this.left = null;
             this.right = null;
         }
     }
-    
-	
-	// atributos de la clase
-	
+
+    // atributos de la clase
     private Node<E> root;
     private int count, cont;
-    
-	// constructor del arbol
+
+    // constructor del arbol
     public TreeSet() {
         root = null;
         count = 0;
     }
-    
-	// implementa el metodo add, sobreescribe el de la clase abstracta
+
+    // implementa el metodo add, sobreescribe el de la clase abstracta
     @Override
-    public boolean add(E e) {        
-        if (root == null) {                      
+    public boolean add(E e) {
+        if (root == null) {
             root = new Node<>(e);
             count++;
             return true;
@@ -71,13 +67,13 @@ public class TreeSet<E extends Comparable<E>> extends AbstractSet<E> {
                     }
                 }
             }
-        }        
+        }
     }
-    
-	// implementa el metodo contains, sobreescribe el de la clase abstracta
+
+    // implementa el metodo contains, sobreescribe el de la clase abstracta
     @Override
     public boolean contains(Object obj) {
-        
+
         if (obj == null
                 || (root != null && obj.getClass() != root.info.getClass())) {
             return false;
@@ -86,8 +82,8 @@ public class TreeSet<E extends Comparable<E>> extends AbstractSet<E> {
         @SuppressWarnings("unchecked")
         Comparable<E> cmp = (Comparable<E>) obj;
         Node<E> p = root;
-        
-        while (p != null) {            
+
+        while (p != null) {
             if (cmp.compareTo(p.info) == 0) {
                 return true;
             }
@@ -99,75 +95,74 @@ public class TreeSet<E extends Comparable<E>> extends AbstractSet<E> {
         }
         return false;
     }
-    
-	
-	// recorrido inOrder, actualiza una lista con los nodos 
+
+    // recorrido inOrder, actualiza una lista con los nodos 
     private void inOrder(Node<E> root, List<E> lst) {
         if (root != null) {
             inOrder(root.left, lst);
             lst.add(root.info);
             inOrder(root.right, lst);
-        }        
+        }
     }
-    
-	// recorrido inOrder, regresa una lista con los nodos
+
+    // recorrido inOrder, regresa una lista con los nodos
     public List<E> inOrderList() {
         List<E> lst = new ArrayList<>();
         inOrder(root, lst);
         return lst;
     }
 
-	// recorrido preOrder, actualiza una lista con los nodos 
+    // recorrido preOrder, actualiza una lista con los nodos 
     private void preOrder(Node<E> root, List<E> lst) {
         if (root != null) {
             lst.add(root.info);
-            preOrder(root.left, lst);            
+            preOrder(root.left, lst);
             preOrder(root.right, lst);
-        }        
+        }
     }
-    
-	// recorrido preOrder, regresa una lista con los nodos
+
+    // recorrido preOrder, regresa una lista con los nodos
     public List<E> preOrderList() {
         List<E> lst = new ArrayList<>();
         preOrder(root, lst);
         return lst;
     }
-    
-	// recorrido postOrder, actualiza una lista con los nodos 
+
+    // recorrido postOrder, actualiza una lista con los nodos 
     private void postOrder(Node<E> root, List<E> lst) {
-        if (root != null) {            
-            postOrder(root.left, lst);            
+        if (root != null) {
+            postOrder(root.left, lst);
             postOrder(root.right, lst);
             lst.add(root.info);
-        }        
+        }
     }
-    
-	// recorrido postOrder, regresa una lista con los nodos
+
+    // recorrido postOrder, regresa una lista con los nodos
     public List<E> postOrderList() {
         List<E> lst = new ArrayList<>();
         postOrder(root, lst);
         return lst;
     }
-    
-	// implementa el metodo iterator, sobreescribe el de la clase abstracta
+
+    // implementa el metodo iterator, sobreescribe el de la clase abstracta
     @Override
     public Iterator<E> iterator() {
         return inOrderList().iterator();
     }
 
-	// implementa el metodo size, sobreescribe el de la clase abstracta	
+    // implementa el metodo size, sobreescribe el de la clase abstracta	
     @Override
     public int size() {
         return count;
     }
-    
-	// implementa el metodo clear, sobreescribe el de la clase abstracta
+
+    // implementa el metodo clear, sobreescribe el de la clase abstracta
     @Override
     public void clear() {
         root = null;
         count = 0;
     }
-    
+
     //Contador de hojas
     public int CantidadHojas() {
         cont = 0;
@@ -184,21 +179,44 @@ public class TreeSet<E extends Comparable<E>> extends AbstractSet<E> {
             CanHojas(r.right);
         }
     }
+
     //Metodo isFull
-    public boolean isFull(){
-        return(isFull(root));
+    public boolean isFull() {
+        return (isFull(root));
     }
-    
-    private boolean isFull(Node<E> r){
-        if(r == null)
+
+    private boolean isFull(Node<E> r) {
+        if (r == null) {
             return true;
-              
-        if(r.left == null && r.right == null)
+        }
+
+        if (r.left == null && r.right == null) {
             return true;
-        
-        if(r.left != null && r.right != null)
-            return(isFull(r.left) && isFull(r.right));
-        
+        }
+
+        if (r.left != null && r.right != null) {
+            return (isFull(r.left) && isFull(r.right));
+        }
+
+        return false;
+    }
+
+    //Arbol Degenerado
+    public boolean isDegenerate() {
+        return (IsDegenerate(root));
+    }
+
+    private boolean IsDegenerate(Node<E> r) {
+        if (r.left == null && r.right != null) {
+            return (IsDegenerate(r.right));
+        } else if (r.left != null && r.right == null) {
+            return (IsDegenerate(r.left));
+        }
+
+        if (r.left == null && r.right == null) {
+            return true;
+        }
+
         return false;
     }
 
